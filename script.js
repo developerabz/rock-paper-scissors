2bd3d1a2ebde98e810eccaa6be434f8de2cb9733
Abdullah Yakub <abdullah.yakub@hotmail.com>
2022-05-21 23:06:09 +1000

    print on screen player and computer move

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 // creating function called computerPlay
 // function random returns either rock, paper or scissors
 
-function computerPlay(hand) {
-    
-    hand = Math.floor(Math.random()*3 + 1);
-
-        if (hand === 1) {
-            return "Rock"
-        } else if (hand === 2) {
-            return "Paper"
-        } else {
-            return "Scissors"
-        }
-    
+const maincontainer = document.querySelector(".maincontainer");
+const container = document.createElement('div');
+container.classList.toggle("container");
+maincontainer.appendChild(container);
 
 
+
+function computerPlay() {
+    const computermove = document.createElement('p');
+    computermove.classList.toggle("computermove");
+    let hand = Math.floor(Math.random()*3 + 1);
     
+    let allrps = ["Rock", "Paper", "Scissors"];
+    computermove.textContent = allrps[hand - 1];
+    container.appendChild(computermove);
+    return allrps[hand - 1];
 }
+
+
+
 
 
 // function that plays a round with computer
@@ -26,34 +30,102 @@ function computerPlay(hand) {
 // returns string saying you lose or you win
 // player selection is case insensitive
 
+
+
+
+let computerSelection = computerPlay;
+
 function oneRound(playerSelection, computerSelection) {
-    
+    const round = document.createElement('p');
+    round.classList.add("round");
 
-    if (playerSelection.toLowerCase() === "rock" && computerSelection === "Rock") {
-        return `Tie by rock, you = ${playerPoint} computer = ${computerPoint}`;
-    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "Paper") {
-        return `Tie by paper, you = ${playerPoint} computer = ${computerPoint}`;
-    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "Scissors") {
-        return `Tie by Scissors, you = ${playerPoint} computer = ${computerPoint}`;
-    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "Paper") {
-        return `Lose to paper, you = ${playerPoint} computer = ${++computerPoint}`;
-    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "Scissors") {
-        return `Lose to scissors, you = ${playerPoint} computer = ${++computerPoint}`;
-    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "Rock") {
-        return `Lose to rock, you = ${playerPoint} computer = ${++computerPoint}`;
-    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "Scissors") {
-        return `Win with rock, you = ${++playerPoint} computer = ${computerPoint}`;
-    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "Rock") {
-        return `Win with paper, you = ${++playerPoint} computer = ${computerPoint}`;
-    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "Paper") {
-        return `Win with scissors, you = ${++playerPoint} computer = ${computerPoint}`;
+    if (playerSelection === "rock" && computerSelection === "Rock") {
+        round.textContent = `Tie by rock, you = ${playerPoint} computer = ${computerPoint}`;
+        container.appendChild(round);
+    } else if (playerSelection === "paper" && computerSelection === "Paper") {
+        round.textContent = `Tie by paper, you = ${playerPoint} computer = ${computerPoint}`;
+        container.appendChild(round);
+    } else if (playerSelection === "scissors" && computerSelection === "Scissors") {
+        round.textContent = `Tie by Scissors, you = ${playerPoint} computer = ${computerPoint}`;
+        container.appendChild(round);
+    } else if (playerSelection === "rock" && computerSelection === "Paper") {
+        round.textContent = `Lose to paper, you = ${playerPoint} computer = ${++computerPoint}`;
+        container.appendChild(round);
+    } else if (playerSelection === "paper" && computerSelection === "Scissors") {
+        round.textContent = `Lose to scissors, you = ${playerPoint} computer = ${++computerPoint}`;
+        container.appendChild(round);
+    } else if (playerSelection === "scissors" && computerSelection === "Rock") {
+        round.textContent = `Lose to rock, you = ${playerPoint} computer = ${++computerPoint}`;
+        container.appendChild(round);
+    } else if (playerSelection === "rock" && computerSelection === "Scissors") {
+        round.textContent = `Win with rock, you = ${++playerPoint} computer = ${computerPoint}`;
+        container.appendChild(round);
+    } else if (playerSelection === "paper" && computerSelection === "Rock") {
+        round.textContent = `Win with paper, you = ${++playerPoint} computer = ${computerPoint}`;
+        container.appendChild(round);
+    } else if (playerSelection === "scissors" && computerSelection === "Paper") {
+        round.textContent = `Win with scissors, you = ${++playerPoint} computer = ${computerPoint}`;
+        container.appendChild(round);
     }
-
+    container.appendChild(round);
 }
 
-    let playerSelection = prompt("Choose Rock, Paper or Scissors!", "");
 
-    let computerSelection = computerPlay();
+
+
+const buttons = document.querySelectorAll('button');
+
+
+
+
+
+
+
+    let playerSelection = "";
+
+    buttons.forEach((button) => {
+    
+        button.addEventListener('click', () => {
+            if (button.classList.contains("rock")) {
+                playerSelection = "rock";
+                const playermover = document.createElement('p');
+                playermover.classList.toggle("playermover");
+                playermover.textContent = "rock";
+                container.appendChild(playermover);
+                console.log(playerSelection === "rock");
+                
+                if (playerSelection === "rock") {
+                    console.log(computerSelection());
+                    console.log(oneRound(playerSelection, computerSelection));
+                }
+            } else if (button.classList.contains("paper")) {
+                playerSelection = "paper";
+                const playermovep = document.createElement('p');
+                playermovep.classList.toggle("playermovep");
+                playermovep.textContent = "paper";
+                container.appendChild(playermovep);
+                
+                if (playerSelection === "paper") {
+                    computerSelection();
+                    oneRound(playerSelection, computerSelection);
+                }
+            } else if (button.classList.contains("scissors")) {
+                playerSelection = "scissors";
+                const playermoves = document.createElement('p');
+                playermoves.classList.toggle("playermoves");
+                playermoves.textContent = "scissors";
+                container.appendChild(playermoves);
+                
+                if (playerSelection === "scissors") {
+                    computerSelection();
+                    oneRound(playerSelection, computerSelection);
+                }
+            }
+        });
+    
+    });
+
+    
 
 
 //create new function game(). call playRound inside this
@@ -87,5 +159,9 @@ let computerPoint = 0;
 }
 
 console.log(game());*/
+
+// create button that returns name on button in console
+
+
 
 
